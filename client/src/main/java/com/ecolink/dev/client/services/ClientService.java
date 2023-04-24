30c735abd8dabbbe1d1dccbdbd6d71bae279672c
Marquezv <vmarquezv@outlook.com.br>
2023-04-24 20:35:16 -0300

    to_fix: refactor ClientService

--- client/src/main/java/com/ecolink/dev/client/services/ClientService.java
+++ client/src/main/java/com/ecolink/dev/client/services/ClientService.java
@@ -1,53 +1,16 @@
 package com.ecolink.dev.client.services;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.ecolink.dev.client.domain.User;
 
-@Getter
-@Setter
-public class ClientService implements ClientServiceImpl{
+public interface ClientService {
 	
 	private Socket socket;
-	private BufferedWriter bufferedWriter;
-	private BufferedReader bufferedReader;
 	
-	public ClientService(Socket socket) throws IOException {
-		super();
-		this.socket = socket;
-		this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-		this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	}
-	
-	@Override
-	public void sendMessage(String message) {
-		try {
-			bufferedWriter.write(message);
-			bufferedWriter.newLine();
-			bufferedWriter.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public List<String> listOnline() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<String> listAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	void sendString(String message);
+	List<User> findAll();
+	List<User> findOnline();
 	
 }
--- client/src/main/java/com/ecolink/dev/client/services/ClientServiceImpl.java
+++ client/src/main/java/com/ecolink/dev/client/services/ClientServiceImpl.java
@@ -1,12 +1,33 @@
 package com.ecolink.dev.client.services;
 
-import java.io.BufferedWriter;
 import java.util.List;
 
-public interface ClientServiceImpl {
-	
-	public void sendMessage(String message);
-	public List<String> listOnline();
-	public List<String> listAll();
-}
+import com.ecolink.dev.client.domain.User;
 
+public class ClientServiceImpl implements ClientService {
+
+	@Override
+	public void sendString(String message) {
+		try {
+			bufferedWriter.write(message);
+			bufferedWriter.newLine();
+			bufferedWriter.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> findOnline() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
